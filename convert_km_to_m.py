4f9f84094c4c815bd0492fcb3ab2bafba5014631
Nuramaninaa <60954922+Nuramaninaa@users.noreply.github.com>
2023-07-18 19:30:10 +0800

    Add files via upload

---
+++ convert_km_to_m.py
@@ -0,0 +1,9 @@
+#python program to convert km(kilometres) to m(miles)
+
+km1 = float(input("Enter the speed of motorcycle : "))
+
+conversion_ratio = 0.62137
+
+m1 = km1 * conversion_ratio
+
+print("Speed of motorcycle is ", m1) 
